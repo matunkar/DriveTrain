0223246dc3100af9223967d0f545a807f50f7def
Maryam A. Tunkara <tunkarama@1038.org>
2023-11-18 11:30:26 -0500

    erm

---
+++ DriveTrain/src/main/java/frc/robot/constants/DriveConstants.java
@@ -0,0 +1,50 @@
+package frc.robot.constants;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.util.Units;
+
+public final class DriveConstants {
+    // Driving Parameters - Note that these are not the maximum capable speeds of
+    // the robot, rather the allowed maximum speeds
+    public static final double kMaxSpeedMetersPerSecond = 4.8;
+    public static final double kMaxAngularSpeed = 2 * Math.PI; // radians per second
+    public static final double kFineAdjustmentPercent = 0.2;
+
+    // Rotation PID Constants
+    public static final double kRotationLockP = 0.01;
+    public static final double kRotationLockI = 0.000;
+    public static final double kRotationLockD = 0.000;
+    public static final double kRotationLockTolerance = 1.0;
+
+    // Chassis configuration
+    public static final double kTrackWidth = Units.inchesToMeters(26);
+    // Distance between centers of right and left wheels on robot
+    public static final double kWheelBase = Units.inchesToMeters(26);
+    // Distance between front and back wheels on robot
+    public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
+            new Translation2d(kWheelBase / 2, kTrackWidth / 2),
+            new Translation2d(kWheelBase / 2, -kTrackWidth / 2),
+            new Translation2d(-kWheelBase / 2, kTrackWidth / 2),
+            new Translation2d(-kWheelBase / 2, -kTrackWidth / 2));
+
+    // Angular offsets of the modules relative to the chassis in radians
+    public static final double kFrontLeftChassisAngularOffset = -Math.PI / 2;
+    public static final double kFrontRightChassisAngularOffset = 0;
+    public static final double kBackLeftChassisAngularOffset = Math.PI;
+    public static final double kBackRightChassisAngularOffset = Math.PI / 2;
+
+    // SPARK MAX CAN IDs
+    public static final int kBackLeftDrivingCanId = 2;
+    public static final int kBackRightDrivingCanId = 4;
+    public static final int kFrontRightDrivingCanId = 6;
+    public static final int kFrontLeftDrivingCanId = 8;
+
+    public static final int kBackLeftTurningCanId = 1;
+    public static final int kBackRightTurningCanId = 3;
+    public static final int kFrontRightTurningCanId = 5;
+    public static final int kFrontLeftTurningCanId = 7;
+
+    public static final boolean kGyroReversed = false;
+    public static final int kGyroCanId = 22;
+}
--- DriveTrain/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ DriveTrain/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,13 +1,17 @@
 package frc.robot.subsystems;
 
 import frc.robot.constants.NeoMotorConstants;
+import frc.robot.constants.DriveConstants;
 import frc.robot.libraries.MAXSwerveModule;
+import java.math.*;
 
 public class DriveTrain {
-    private final MAXSwerveModule frontLeft = new MAXSwerveModule(0, 0, 0)
-    private final MAXSwerveModule frontRight = new MAXSwerveModule(0, 0, 0)
-    private final MAXSwerveModule backLeft = new MAXSwerveModule(0, 0, 0)
-    private final MAXSwerveModule backRight = new MAXSwerveModule(0, 0, 0)
+
+
+    private final MAXSwerveModule frontLeft = new MAXSwerveModule(DriveConstants.kFrontLeftDrivingCanId, DriveConstants.kFrontLeftTurningCanId, DriveConstants.kFrontLeftChassisAngularOffset);
+    private final MAXSwerveModule frontRight = new MAXSwerveModule(DriveConstants.kFrontRightDrivingCanId, DriveConstants.kFrontRightTurningCanId, DriveConstants.kFrontRightChassisAngularOffset);
+    private final MAXSwerveModule backLeft = new MAXSwerveModule(DriveConstants.kBackLeftDrivingCanId, DriveConstants.kBackLeftTurningCanId, DriveConstants.kBackLeftChassisAngularOffset);
+    private final MAXSwerveModule backRight = new MAXSwerveModule(DriveConstants.kBackRightDrivingCanId, DriveConstants.kBackRightTurningCanId, DriveConstants.kBackRightChassisAngularOffset);
 
     private static DriveTrain INSTANCE;
 
@@ -16,5 +20,16 @@ public class DriveTrain {
             INSTANCE = new DriveTrain();
         }
         return INSTANCE;
+    }
+
+    public void setModuleStates() {
+
+    }
+
+    public void drive(double xSpeed, double ySpeed, double rotation) {
+        xSpeed *= DriveConstants.kMaxSpeedMetersPerSecond;
+        ySpeed *= DriveConstants.kMaxSpeedMetersPerSecond;
+        rotation *= DriveConstants.kMaxAngularSpeed;
+
     }
 }
